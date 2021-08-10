3b74ab64f1a300f330838408d4a15e10f1c544ca
sufail <sufailps786@gmail.com>
2021-08-10 22:56:39 +0530

    adding calc file

---
+++ a.py
@@ -0,0 +1,6 @@
+num1 = int(input("First number"))
+num2 = int(input(" Second number"))
+
+sum = num1 + num2
+
+print(sum)
---
+++ multiplication.py
@@ -0,0 +1,5 @@
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number"))
+result = num1 * num2
+print(result)
